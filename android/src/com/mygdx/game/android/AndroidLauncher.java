b36877749a6249292a7b67bc0cced1c7735b81e6
Ariana Kaviani <a.kaviani440@gmail.com>
2015-11-26 18:04:00 -0500

    dank again

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.game.MenuScratch;
+import com.mygdx.game.ScreenHandler;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MenuScratch(), config);
+		initialize(new ScreenHandler(), config);
 	}
 }
